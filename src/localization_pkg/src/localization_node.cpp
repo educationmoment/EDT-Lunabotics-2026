e3bb820d22ca616b4081f4a753d37987e4aed7de
cdiede2 <cdiede2@uic.edu>
2025-02-10 11:17:24 -0600

    Refactor localization node: should output up to velocity

--- src/localization_pkg/src/localization_node.cpp
+++ src/localization_pkg/src/localization_node.cpp
@@ -1,44 +1,24 @@
 #include    "rclcpp/rclcpp.hpp"
 // #include    "std_msgs/msg/Float32MultiArray"
 #include    "geometry_msgs/msg/accel_stamped.hpp"
+#include    "rclcpp/time.hpp"
 #include    <iostream>
 #include    <memory>
 
 
 // typedef unsigned long time_t;    // Size of time_t: 64 bis (8-Bytes)
 
+
 class LocalizationNode : public rclcpp::Node {
 public:
     LocalizationNode() : Node("localization_node") {
-        RCLCPP_INFO( this->get_logger(), "Test from Localization Node" );
-
-        RCLCPP_INFO( this->get_logger(), "Initiallizing stream");
-        this->stream.accel.x = 0.0;
-        this->stream.accel.y = 0.0;
-        this->stream.accel.z = 0.0;
-        this->stream.gyro.x = 0.0;
-        this->stream.gyro.y = 0.0;
-        this->stream.gyro.z = 0.0;
+        RCLCPP_INFO( this->get_logger(), "Localization Node Startup" );
 
         RCLCPP_WARN( this->get_logger(), "Size of..." );
         RCLCPP_WARN( this->get_logger(), "\tINT: %lu", sizeof(int));
         RCLCPP_WARN( this->get_logger(), "\tlong INT: %lu", sizeof(long int));
         RCLCPP_WARN( this->get_logger(), "\tFLOAT: %lu", sizeof(float));
         RCLCPP_WARN( this->get_logger(), "\tDOUBLE: %lu", sizeof(double));
-
-
-
-
-
-        RCLCPP_INFO( this->get_logger(), "Accel Stream");
-        RCLCPP_INFO( this->get_logger(), "\tX: %0.2lf", this->stream.accel.x);
-        RCLCPP_INFO( this->get_logger(), "\tY: %0.2lf", this->stream.accel.y);
-        RCLCPP_INFO( this->get_logger(), "\tZ: %0.2lf", this->stream.accel.z);
-
-        RCLCPP_INFO( this->get_logger(), "\tX: %0.2lf", this->stream.gyro.x);
-        RCLCPP_INFO( this->get_logger(), "\tY: %0.2lf", this->stream.gyro.y);
-        RCLCPP_INFO( this->get_logger(), "\tZ: %0.2lf", this->stream.gyro.z);
-        
 
         // Create Subscriber to IMU Data
         //////////////////////////////////////
@@ -63,60 +43,74 @@ private:
     struct {
         // Acceleration Data
         struct {
-            double x;
-            double y;
-            double z;
-        } accel;
+            double x, y, z;
+        } linear_accel, angular_accel;
 
-        // Gyroscope Data
+        // Velocity Data
         struct {
-            double x;
-            double y;
-            double z;
-        } gyro;
+            double x, y, z;
+        } linear_vel, angular_vel;
 
+        // Displacement Data
+        struct {
+            double x, y, z;
+        } linear_disp, angular_disp;
         time_t time_stamp;
-    } stream;
+    } state, past_state;
+
+    // Static Variable to hold old timestamp
+    ////////////////////
+    static rclcpp::Time old_time;
     
+    // Callback function: Handles received IMU frames
+    ////////////////////////////////////////
     void imu_callback( const geometry_msgs::msg::AccelStamped::SharedPtr msg ) {
-        static double oldAccel_y = 0; 
-        static unsigned int old_time = 0;
-        
-        if ( !old_time ) {
-            old_time = msg->header.stamp.nanosec;
+        rclcpp::Time current_time = msg->header.stamp;
+        double dt = (current_time - old_time).seconds();
+
+        if ( ! old_time.nanoseconds() ) {
+            old_time = current_time;
             return;
         } 
         
-        if ( old_time == (unsigned long long)msg->header.stamp.nanosec ) {
-            return;
-        }
-        
-        trapezoidal_rule( this->stream.accel.y, oldAccel_y, msg->accel.linear.y, msg->header.stamp.nanosec - old_time);
-        oldAccel_y = msg->accel.linear.y;
-        
-        
-        RCLCPP_INFO( this->get_logger(), "Received IMU Data at timestamp: %u", msg->header.stamp.nanosec);
-        RCLCPP_INFO( this->get_logger(), "Velocity Y: %0.2f", this->stream.accel.y);
+        // Calculate Linear Velocity
+        trapezoidal_rule( this->state.linear_vel.x, this->past_state.linear_accel.x, msg->accel.linear.x, dt );
+        trapezoidal_rule( this->state.linear_vel.y, this->past_state.linear_accel.y, msg->accel.linear.y, dt );
+        trapezoidal_rule( this->state.linear_vel.z, this->past_state.linear_accel.z, msg->accel.linear.z, dt );
+        this->past_state.linear_accel.x = this->state.linear_accel.x; 
+        this->past_state.linear_accel.y = this->state.linear_accel.y;
+        this->past_state.linear_accel.z = this->state.linear_accel.z;
+
+        // Calculate Angular Velocity
+        trapezoidal_rule( this->state.angular_vel.x, this->past_state.angular_accel.x, msg->accel.angular.x, dt );
+        trapezoidal_rule( this->state.angular_vel.y, this->past_state.angular_accel.y, msg->accel.angular.y, dt );
+        trapezoidal_rule( this->state.angular_vel.z, this->past_state.angular_accel.z, msg->accel.angular.z, dt );
+        this->past_state.angular_accel.x = this->state.angular_accel.x;
+        this->past_state.angular_accel.y = this->state.angular_accel.y;
+        this->past_state.angular_accel.z = this->state.angular_accel.z;
+
+        // RCLCPP_INFO( this->get_logger(), "Received IMU Data at timestamp: %u", msg->header.stamp.nanosec);
         return;
     }
     
 
     // Trapezoidal Method
     ////////////////////
-    void trapezoidal_rule( double& stream, double  val_a, double val_b, unsigned int dt) {
-        if( !dt ) { return; }
-        RCLCPP_INFO( this->get_logger(), "\033[104mVal A: \t%0.3lf\033[0m", val_a );
-        RCLCPP_INFO( this->get_logger(), "\033[104mVal B: \t%0.3lf\033[0m", val_b );
-        RCLCPP_INFO( this->get_logger(), "\033[104mDel T: \t%u\033[0m", dt );
-        RCLCPP_INFO( this->get_logger(), "\033[106mVal A: \t%0.3lf\033[0m", 0.5 * ( val_a + val_b) * static_cast<double>(dt) / 1E9 );
+    void trapezoidal_rule( double& stream, double  val_a, double val_b, double dt) {
+        if( dt <= 0.0 ) { return; }
 
+        // RCLCPP_INFO( this->get_logger(), "\033[104mVal A: \t%0.3lf\033[0m", val_a );
+        // RCLCPP_INFO( this->get_logger(), "\033[104mVal B: \t%0.3lf\033[0m", val_b );
+        // RCLCPP_INFO( this->get_logger(), "\033[104mDel T: \t%u\033[0m", dt );
+        // RCLCPP_INFO( this->get_logger(), "\033[106mVal A: \t%0.3lf\033[0m", 0.5 * ( val_a + val_b) * static_cast<double>(dt) / 1E9 );
 
-
-        stream += 0.5 * (val_b + val_a) * static_cast<double>(dt) / 1E9;
+        stream += 0.5 * (val_b + val_a) * dt;
         return;
     }
 
 };
+
+rclcpp::Time LocalizationNode::old_time = rclcpp::Time(0, 0, RCL_ROS_TIME);
 
 
 int main(int argc, char* argv[]) {
